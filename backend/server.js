1153f0084a110e9c43eb5993520702ee4c4d2ea2
Targys <gjtoison8@gmail.com>
2020-02-13 18:33:11 +0100

    referencement

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,8 @@ const port = process.env.PORT || 5000
 
 app.use(cors());
 app.use(express.json());
+app.use(require('prerender-node').set('prerenderToken', 'hOYKP0Da3SG3ydo7wwzT'));
+
 
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }).then(() => {
     console.log('Connected to mongoDB')
